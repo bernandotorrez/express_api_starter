5d989266502c032a485c2a6f332c0197a3cacce6
bernandotorrez <bernandotorrez4@gmail.com>
2021-08-25 09:13:51 +0700

    update

--- exceptions/InvariantError.js
+++ exceptions/InvariantError.js
@@ -2,7 +2,7 @@ const ClientError = require('./ClientError');
 
 class InvariantError extends ClientError {
   constructor(message, statusCode = 200) {
-    super(message);
+    super(message, statusCode);
     this.statusCode = statusCode;
     this.name = 'InvariantError';
   }
